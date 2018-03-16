aa7b938f3b52f3530aff36770dd6988f56a9ffc9
quocnguyencong <hoanglantim542000@gmail.com>
2018-03-16 22:42:48 +0700

    Update UI

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('A test Recipe',
+    'this is a simpli test'
+    // tslint:disable-next-line:max-line-length
+    , 'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=KPTNrvis'
+    ),
+    new Recipe('A test Recipe',
+    'this is a simpli test'
+    // tslint:disable-next-line:max-line-length
+    , 'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=KPTNrvis'
+    ),
+  ];
 
   constructor() { }
 
